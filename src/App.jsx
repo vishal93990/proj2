f04cf53363be5cb4d1ea64d122784633d53ed426
vishal93990 <vjha93011@gmail.com>
2025-06-27 11:31:17 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ const router = createBrowserRouter([
     errorElement: <Error/>,
     children: [
       {
-        path: "/",
+        path: "/proj2",
         element: <Home />,
       },
       {
